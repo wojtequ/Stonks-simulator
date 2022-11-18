12a2d5b4f3fee12c94aef83f5461105147fcabcb
Bartosz Kępa <bartekkepa1999@gmail.com>
2022-11-18 16:19:56 +0100

    added responsive card to homepage

--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -1,80 +1,93 @@
 import {
-  Button,
-  Center,
-  Container,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  SimpleGrid,
-} from "@chakra-ui/react";
-import { Fragment, useState } from "react";
-import { t } from "../../translations/utils";
-import { BallanceCard } from "../BallanceCard";
-import { BallanceManagmentButtonGroup } from "../BallanceManagmentButtonGroup";
+	Button,
+	Center,
+	Container,
+	Flex,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
+	NumberDecrementStepper,
+	NumberIncrementStepper,
+	NumberInput,
+	NumberInputField,
+	NumberInputStepper,
+	SimpleGrid,
+	Box,
+} from '@chakra-ui/react';
+import { Fragment, useState } from 'react';
+import { t } from '../../translations/utils';
+import { BallanceCard } from '../BallanceCard';
+import { BallanceManagmentButtonGroup } from '../BallanceManagmentButtonGroup';
 
 export const HomePage = () => {
-  const [isAddFoundsModalOpened, setIsAddFoundsModalOpened] =
-    useState<boolean>(false);
-  return (
-    <Fragment>
-      <Center mt={10}>
-        <Container maxW="2xl">
-          <SimpleGrid
-            minChildWidth="312px"
-            columns={2}
-            spacing={4}
-            alignItems="center"
-            justifyContent="center"
-          >
-            <BallanceCard />
-            <BallanceManagmentButtonGroup
-              openAddFounds={() => setIsAddFoundsModalOpened(true)}
-            />
-            <Modal
-              isOpen={isAddFoundsModalOpened}
-              isCentered
-              onClose={() => setIsAddFoundsModalOpened(false)}
-            >
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>
-                  <ModalCloseButton />
-                </ModalHeader>
-                <ModalBody padding={"10%"}>
-                  {/* <Flex> */}
-                  <Flex direction={"column"}>
-                    <NumberInput allowMouseWheel>
-                      <NumberInputField
-                        placeholder={t(
-                          "add-funds.modal.number-input-placeholder"
-                        )}
-                      />
-                      <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
-                      </NumberInputStepper>
-                    </NumberInput>
-                    <Button colorScheme="blue" width="100%" marginTop="10%">
-                      {t("add-funds.modal.add-button")}
-                    </Button>
-                    {/* </Flex> */}
-                    {/* <BallanceCard /> */}
-                  </Flex>
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </SimpleGrid>
-        </Container>
-      </Center>
-    </Fragment>
-  );
+	const [isAddFoundsModalOpened, setIsAddFoundsModalOpened] =
+		useState<boolean>(false);
+	return (
+		<Fragment>
+			<Center mt={10}>
+				<Container maxW='2xl'>
+					<SimpleGrid
+						minChildWidth='312px'
+						columns={2}
+						spacing={4}
+						alignItems='center'
+						justifyContent='center'
+					>
+						<BallanceCard />
+						<BallanceManagmentButtonGroup
+							openAddFounds={() => setIsAddFoundsModalOpened(true)}
+						/>
+						<Modal
+							isOpen={isAddFoundsModalOpened}
+							isCentered
+							onClose={() => setIsAddFoundsModalOpened(false)}
+							size='4xl'
+						>
+							<ModalOverlay />
+							<ModalContent>
+								<ModalHeader>
+									<ModalCloseButton />
+								</ModalHeader>
+								<ModalBody padding={'10%'}>
+									{/* <Flex> */}
+									<SimpleGrid
+										minChildWidth='312px'
+										columns={2}
+										spacing={4}
+										alignItems='center'
+										justifyContent='center'
+									>
+										<Box justifySelf='center' maxW='sm' minW='310px'>
+											<Flex direction={'column'}>
+												<NumberInput allowMouseWheel>
+													<NumberInputField
+														placeholder={t(
+															'add-funds.modal.number-input-placeholder'
+														)}
+													/>
+													<NumberInputStepper>
+														<NumberIncrementStepper />
+														<NumberDecrementStepper />
+													</NumberInputStepper>
+												</NumberInput>
+												<Button colorScheme='blue' width='100%' marginTop='10%'>
+													{t('add-funds.modal.add-button')}
+												</Button>
+											</Flex>
+										</Box>
+										<BallanceCard />
+									</SimpleGrid>
+									{/* </Flex> */}
+									{/* <BallanceCard /> */}
+								</ModalBody>
+							</ModalContent>
+						</Modal>
+					</SimpleGrid>
+				</Container>
+			</Center>
+		</Fragment>
+	);
 };
