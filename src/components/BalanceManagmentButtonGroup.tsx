e64baf5063ab0e85e175040ee72c2f71d5e265c9
Bartosz Kępa <bartekkepa1999@gmail.com>
2023-01-02 16:04:27 +0100

    added redirection on buttons at homepage

--- src/components/BalanceManagmentButtonGroup.tsx
+++ src/components/BalanceManagmentButtonGroup.tsx
@@ -4,6 +4,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { BsWallet2 } from "react-icons/bs";
 import { TbArrowsDownUp } from "react-icons/tb";
 import { t } from "../translations/utils";
+import { NavLink } from "react-router-dom";
 
 const ButtonStyle = {
   width: "105px",
@@ -21,8 +22,10 @@ export const BalanceManagmentButtonGroup: React.FC<
     <ButtonGroup justifySelf="center">
       <Button style={ButtonStyle}>
         <VStack>
-          <Text fontSize="sm">Transfer</Text>
-          <Icon w={6} h={6} as={TbArrowsDownUp} />
+          <NavLink to="/transactions">
+            <Text fontSize="sm">Transfer</Text>
+            <Icon mt={2} w={6} h={6} as={TbArrowsDownUp} />
+          </NavLink>
         </VStack>
       </Button>
       <Button style={ButtonStyle} onClick={openAddFounds}>
@@ -33,8 +36,10 @@ export const BalanceManagmentButtonGroup: React.FC<
       </Button>
       <Button style={ButtonStyle}>
         <VStack>
-          <Text fontSize="sm">{t("details")}</Text>
-          <Icon w={6} h={6} as={BsWallet2} />
+          <NavLink to="/details">
+            <Text fontSize="sm">{t("details")}</Text>
+            <Icon mt={2} w={6} h={6} as={BsWallet2} />
+          </NavLink>
         </VStack>
       </Button>
     </ButtonGroup>
