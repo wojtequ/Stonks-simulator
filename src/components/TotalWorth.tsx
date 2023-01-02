12660e4843af42f406f78420d3f220ae7fda8c55
wojtequ <wojtequ@gmail.com>
2023-01-02 16:16:05 +0100

    added toFixed for some numbers

--- src/components/TotalWorth.tsx
+++ src/components/TotalWorth.tsx
@@ -1,23 +1,24 @@
-import { Box, Text, Highlight } from '@chakra-ui/react'
-import { t } from '../translations/utils'
+import { Box, Highlight, Text } from "@chakra-ui/react";
+import { t } from "../translations/utils";
 
 export const TotalWorth: React.FC<{ worth: number }> = ({ worth }) => {
-    return (
-        <Box backgroundColor="#EFEFEF"
-            borderRadius="7px"
-            paddingLeft="10px"
-            paddingRight="10px"
-            borderWidth="3px"
-            borderColor="black"
-            color="black"
-            height="fit-content"
-            p={2}
-        >
-            <Text>
-                <Highlight query={t("total-worth")} styles={{ fontWeight: "bold" }}>
-                    {`${t("total-worth")}: $${worth}`}
-                </Highlight>
-            </Text>
-        </Box>
-    )
-}
+  return (
+    <Box
+      backgroundColor="#EFEFEF"
+      borderRadius="7px"
+      paddingLeft="10px"
+      paddingRight="10px"
+      borderWidth="3px"
+      borderColor="black"
+      color="black"
+      height="fit-content"
+      p={2}
+    >
+      <Text>
+        <Highlight query={t("total-worth")} styles={{ fontWeight: "bold" }}>
+          {`${t("total-worth")}: $${worth.toFixed(2)}`}
+        </Highlight>
+      </Text>
+    </Box>
+  );
+};
